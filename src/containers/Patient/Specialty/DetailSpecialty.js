6b3476768aac3d38405fd24076bbb17309b4778b
ducduy1102 <ducduy1110u@gmail.com>
2024-08-15 23:25:23 +0700

    View detail specialty

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -5,19 +5,53 @@ import { useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import "./DetailSpecialty.scss";
 import HomeHeader from "../../Home/HomeHeader";
+import DoctorSchedule from "../Doctor/DoctorSchedule";
+import DoctorExtraInfor from "../Doctor/DoctorExtraInfor";
+import ProfileDoctor from "../Doctor/ProfileDoctor";
 
 const DetailSpecialty = (props) => {
   const language = useSelector((state) => state.app.language);
-
+  const arrDoctorId = [122, 131, 134, 135];
   useEffect(() => {
     // Logic when language changes if needed
   }, [language]);
 
   return (
-    <>
+    <div className="detail-specialty-container">
       <HomeHeader isShowBanner={false} />
-      <div className="doctor-detail-container">Hello</div>
-    </>
+      <div className="description-specialty">
+        <div className="container">hi</div>
+      </div>
+      <div className="child-doctor-container">
+        <div className="container">
+          {arrDoctorId &&
+            arrDoctorId.length > 0 &&
+            arrDoctorId.map((item, index) => {
+              return (
+                <div className="child-doctor" key={`detail-doctor-${index}`}>
+                  <div className="detail-content-left">
+                    <div className="profile-doctor">
+                      <ProfileDoctor
+                        doctorId={item}
+                        isShowDescriptionDoctor={true}
+                        // dataTime={dataTime}
+                      />
+                    </div>
+                  </div>
+                  <div className="detail-content-right">
+                    <div className="doctor-schedule">
+                      <DoctorSchedule doctorIdFromParent={item} />
+                    </div>
+                    <div className="doctor-extra-infor">
+                      <DoctorExtraInfor doctorIdFromParent={item} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
   );
 };
 
